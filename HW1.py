b2b0aa70c643744eb3e31f22d93e882510f921c6
bmilenki <bmilenki@gmail.com>
2021-01-30 16:59:19 -0500

    Add files via upload

--- HW1.py
+++ HW1.py
@@ -1,20 +1,22 @@
 import numpy as np
 import os
-import copy
+import cv2
 from PIL import Image
 from scipy import stats
 from matplotlib import pyplot as plt
-import cv2
+
 
 
 def main():
-    # Question1()
+    Question1()
     facesMatrix1600, featureMeans, featureStdDevs, eigenValues, eigenVectors = Question2()
-    # Question3(facesMatrix1600, featureMeans, featureStdDevs, eigenValues, eigenVectors)
+    Question3(facesMatrix1600, featureMeans, featureStdDevs, eigenValues, eigenVectors)
 
 
 def Question1():
     # Starting part C
+
+    print("Question 1 Start:----------------------------------------")
 
     X_org = np.array([[0, 1], [0, 0], [1, 1], [0, 0], [1, 1], [1, 0], [1, 0], [1, 1], [2, 0], [2, 1]])
 
@@ -27,20 +29,14 @@ def Question1():
     # splitting so we can zscore based on column
     f1, f2 = np.hsplit(X_org, 2)
 
-    # print(f1,f2,f3)
     # using scipy to z score, ddof makes us use n-1
     f1_zscored = stats.zscore(f1, ddof=1)
     f2_zscored = stats.zscore(f2, ddof=1)
-
-    # print(f2_zscored)
 
     # X is now z-scored by column/feature
     X = np.concatenate((f1_zscored, f2_zscored), 1)
 
     print("Z Scored X: \n", X)
-
-    # print(X)
-    # print(stats.zscore(np.array([1.3147, 1.4058, 1.6270, 1.4134, 3.1324, 1.5975]),ddof=1))
 
     covX = np.cov(np.transpose(X), ddof=1)
 
@@ -61,8 +57,10 @@ def Question1():
     newX = np.matmul(X_org, ev1)
     print("newX:\n", newX)
 
+    print("Question 1 End:----------------------------------------")
 
 def Question2():
+    print("Question 2 Start:----------------------------------------")
     facesNameList = []
     facesMatrix1600 = []
 
@@ -83,17 +81,13 @@ def Question2():
     # opening every file in the list, resizing them, flattening them, and saving to final list
     for facesName in facesNameList:
         image = Image.open(facesName)
-        # image.show()
-        # imageData = np.asarray(image)
         imageResized = image.resize((40, 40))
-        imageResizedData = np.asarray(imageResized)
+        imageResizedData = np.array(imageResized)
         imageResizedData = imageResizedData.astype("float64")
         flattenImageData = imageResizedData.flatten(order="C")
 
         facesMatrix1600.append(flattenImageData)
 
-    # print(facesMatrix1600)
-    orgFacesMatrix1600 = copy.deepcopy(facesMatrix1600)
     facesMatrix1600 = np.array(facesMatrix1600)
 
     # array to standardize
@@ -104,133 +98,98 @@ def Question2():
         for j in range(len(facesMatrix1600)):  # loops 154 times for each image
             currPixel.append(facesMatrix1600[j][i])
 
-        # print(currPixel)
-
         meanCurrPixel = np.mean(currPixel)
         stdDevCurrPixel = np.std(currPixel, ddof=1)
 
         featureMeans.append(meanCurrPixel)
         featureStdDevs.append(stdDevCurrPixel)
 
-        # print(meanCurrPixel)
-        # print(stdDevCurrPixel)
-
         # now we can standardize
         for k in range(len(facesMatrix1600)):  # loops for each image
             facesMatrix1600[k][i] = (facesMatrix1600[k][i] - meanCurrPixel) / stdDevCurrPixel
 
     # reducing the data using PCA
-    # print(facesMatrix1600)
-
-    facesMatrix1600 = np.transpose(facesMatrix1600)
-
-    covFaces = np.cov(facesMatrix1600, ddof=1)
+    covFaces = np.cov(np.transpose(facesMatrix1600), ddof=1)
     U, S, V = np.linalg.svd(covFaces)
-    # eigW, eigV = np.linalg.eig(covFaces)
 
     eigenValues = S
-    eigenVectors = U
+    eigenVectors = V
 
-    order = eigenValues.argsort()[::-1]
-    eigenValues = eigenValues[order]
-    eigenVectors = eigenVectors[:, order]
+    # uncomment to see eigenvalues
+    # print("EigenValues:\n", eigenValues)
 
-    print("EigenValues:\n", eigenValues)
-    print(len(eigenValues))
+    # print("EigenVectors:\n", eigenVectors)
 
-    print("EigenVectors:\n", eigenVectors)
-    print(len(eigenVectors))
-
-    # the two largest are just the first two since we ordered it
+    # the two largest are just the first two since the function orders it
     eVal1 = eigenValues[0]
     eVal2 = eigenValues[1]
 
     eVec1 = np.array(eigenVectors[0])
     eVec2 = np.array(eigenVectors[1])
 
-
-    # print(eVec2.dtype)
-    # print(U)
-    # print("eVec1:", eVec1)
-    # print("eVec2:", eVec2)
+    # uncomment to check norms
     # print(np.linalg.norm(eVec1))
     # print(np.linalg.norm(eVec2))
 
     W = np.array([eVec1, eVec2]).transpose()
-    # print(W.dtype)
 
-    Z = np.matmul(orgFacesMatrix1600, W)
-    #Z = np.matmul(facesMatrix1600, W)
-
-    # print(Z)
+    Z = np.matmul(facesMatrix1600, W)
 
     newX, newY = np.hsplit(Z, 2)
 
     plt.scatter(x=newX, y=newY, facecolors='none', edgecolors="blue")
-    # plt.ylim([-2, 5])
-    # plt.xlim([-2, 5])
+    # plt.gca().invert_yaxis() # can we used to flip so we get the same image as in the pdf
     plt.show()
 
-    # print("-----------------------------")
-
-    # print(facesMatrix1600)
-
-    # print("__________________________________")
-    # print(orgFacesMatrix1600)
-
+    print("Question 2 End:----------------------------------------")
     # returns the values needed for Q3
     return facesMatrix1600, featureMeans, featureStdDevs, eigenValues, eigenVectors
 
 def Question3(facesMatrix1600, featureMeans, featureStdDevs, eigenValues, eigenVectors):
+    print("Question 3 Start:----------------------------------------")
     img_array = []
 
     person = facesMatrix1600[0]  # gets subject02.centerlight
 
     for k in range(1, len(person)+1):
-        # print("k = ", k)
         W = eigenVectors[0:k]
-        # print("W: bef",W)
+        # transposing them to columns
         W = np.array(W).transpose()
+
+        # getting our Z
         Z = np.matmul(person, W)
 
-        # print("Z:", Z)
-
-        newX = np.matmul(Z,W.transpose())
-        # print(newX)
+        newX = np.matmul(Z, W.transpose())
 
         # undoes standardization
         for i in range(len(newX)):
             newVal = (newX[i] * featureStdDevs[i])+featureMeans[i]
+            # so the pixel values dont go out of range and become black
             if newVal > 255:
                 newVal = 255
             if newVal < 0:
                 newVal = 0
             newX[i] = newVal
 
-        # print("newX: aft", newX)
-
-
         # unflatten the picture
         newPicData = np.reshape(newX, (40, 40))
         newPicData = newPicData.astype(np.uint8)
-        #newPic = Image.fromarray(np.uint8(newPicData))
-
-        # newPic.show()
 
         img_array.append(newPicData)
+
 
     height, width = img_array[0].shape
     size = (width, height)
 
 
-    out = cv2.VideoWriter('project.avi', cv2.VideoWriter_fourcc(*"DIVX"), 60, size)
+    out = cv2.VideoWriter('Milenki_Assignment1.avi', cv2.VideoWriter_fourcc(*"DIVX"), 60, size)
 
     for j in range(len(img_array)):
         currImage = cv2.cvtColor(img_array[j], cv2.COLOR_GRAY2BGR)
         out.write(currImage)
     out.release()
 
-
+    print("Question 3 End:----------------------------------------")
 
 if __name__ == "__main__":
     main()
---
+++ Readme.txt
@@ -0,0 +1,18 @@
+Readme.txt
+Brian Milenki - 1/30/2021 - CS 383 Assignment 1
+
+File Location:
+An unzipped version of yalefaces folder should be in the same directory as the python script. That is the folder that carries everything, not the actual files themselves.
+The video for part 3 will be saved in the same directory as the python script, in avi format.
+
+Running the Code:
+Each question has its own function that is runnable from the main() function. The default will be to run all 3 questions, and you can comment out the ones you dont want to run. 
+Question 2 MUST be ran if Question 3 is also desired to run as Q3 takes input from the output of Q2.
+
+Dependencies
+numpy
+scipy - used to standardize in Q1 (done manually in Q2)
+matplotlib
+os - for reading the file names from the directory
+opencv-python (cv2) - video creation
+pillow (PIL) - image manipulation
